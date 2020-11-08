a6b58d6c4ef59d1c1b9597672983afb45da5260c
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-11-08 08:53:58 -0500

    scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Portfolio</h1>
+      <Router>
+        {/* <Navbar /> */}
+        <Switch>
+          {/* <Route path="/" exact component={Home} /> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
